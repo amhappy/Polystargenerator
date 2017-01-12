28dbbd0d8c492ef8fa86baf7210db6b630c5abce
amhappy <hoornenborg.m@gmail.com>
2017-01-12 21:13:14 +0100

    this code includes a userinterface
    limited user interface allows to give p for number of points

---
+++ StarUI.py
@@ -0,0 +1,61 @@
+from turtle import *
+import turtle
+
+def angle(p_given):
+    p = p_given
+    if p == 30 or p == 90:
+        a = 720 / p
+    elif p % 2 == 0:
+        a = 360 / p
+    else:
+        a = 180 / p
+
+def setup_screen():
+    clearscreen()
+    bgcolor("pink")
+    pencolor("black")
+    pensize(2)
+    fillcolor("white")
+    hideturtle()
+    speed(10)
+    setheading(0)
+
+def draw_star(a):
+    setup_screen()
+    d = 400
+    setx(- 0.5 * d)
+    r = a + 180
+    pendown()
+    begin_fill()
+    forward(d)
+    right(r)
+    x = 1
+    while turtle.heading() != 0:
+        forward(d)
+        right(r)
+        x = x + 1
+    end_fill()
+    print(int(p), x)
+
+instructions = '''Ik ga een ster voor je tekenen. Hoeveel punten mag de ster hebben?'''
+screen = getscreen()
+while True:
+    p_given = screen.numinput('Sterren tekenen', instructions, 7, 3, 150)
+    if p_given < 3:
+        break
+    else:
+        p = p_given
+        if p % 2 == 0:
+            if p / 2 % 2 == 0:
+                a = 2 * 180 / p
+            else:
+                a = 4 * 180 / p
+        else:
+            a = 180 / p
+        draw_star(a)
+
+
+'''
+ts = turtle.getscreen()
+ts.getcanvas().postscript(file="stars55.eps")
+'''
